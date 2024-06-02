6cca79bbd8ca1f1061091139e6d0d5ac39b7bfd3
adamkhobba <adamekhobba6@gmail.com>
2024-06-02 18:01:05 +0100

    norm + updating check_qoute ft

--- parsing/check_redirections.c
+++ parsing/check_redirections.c
@@ -6,27 +6,60 @@
 /*   By: adam <adam@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/02 08:41:52 by adam              #+#    #+#             */
-/*   Updated: 2024/06/02 08:45:08 by adam             ###   ########.fr       */
+/*   Updated: 2024/06/02 16:42:40 by adam             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../include/minishell.h"
 
 
-int ft_check_redirections(t_link *link)
+int ft_check_redirections(t_link *link, t_command *command)
 {
     int i;
 
     i = 0;
     while (link)
     {
-        if (link->identifer == LESS)
+            if (link->identifer == LESS)
             {
                 if (link->next->identifer == STR)
-                    i++;
-                else
+                {
+                    command->input = ft_strdup(link->next->command);
+                    command = command->next;
+                }
+                else 
                     return (0);
             }            
+            else if (link->identifer == GREAT)
+            {
+                if (link->next->identifer == STR)
+                {
+                    command->output = ft_strdup(link->next->command);
+                    command = command->next;
+                }
+                else 
+                    return (0);
+            }
+            else if (link->identifer == LESSLESS)
+            {
+                if (link->next->identifer == STR)
+                {
+                    command->append = ft_strdup(link->next->command);
+                    command = command->next;
+                }
+                else 
+                    return (0);
+            }
+            else if (link->identifer == GREATGREAT)
+            {
+                if (link->next->identifer == STR)
+                {
+                    command->heredoc = ft_strdup(link->next->command);
+                    command = command->next;
+                }
+                else 
+                    return (0);
+            }
         link = link->next;
     }
     return (0);
